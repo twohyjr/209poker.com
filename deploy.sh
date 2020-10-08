63e117e178cbfdd20d33d2b7c631cbe9cbf32e12
Rick Twohy <twohyjr@gmail.com>
2020-10-08 08:12:08 -0400

    updated package.json

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 cd ./frontend/website
 rm -rf ./dist
 
-ember build --environment=development --output-path=dist/website 
+make build-ci
+
 aws s3 rm s3://209poker.com --recursive
 aws s3 cp ./dist/website s3://209poker.com/ --recursive
